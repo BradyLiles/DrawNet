88b9cc0133f06a37d0ad82392a4ebf045557ef39
Brady Liles <bliles@flextechs.com>
2015-11-13 19:59:11 -0700

    Mid-Development on theme design and creation.

--- DrawNet.Draw/App_Start/BundleConfig.cs
+++ DrawNet.Draw/App_Start/BundleConfig.cs
@@ -14,6 +14,15 @@ namespace DrawNet.Draw
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add( new ScriptBundle( "~/bundles/application" ).Include(
+                "~/Scripts/app.min.js",
+                "~/Scripts/demo.js" ) );
+
+
+            bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
+                "~/Content/plugins/fastclick/fastclick.min.js",
+                "~/Content/plugins/slimScroll/jquery.slimscroll.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +35,10 @@ namespace DrawNet.Draw
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/helpers.css",
+                      "~/Content/plugins/fontAwesome/css/font-awesome.min.css",
+                      "~/Content/plugins/ionicons/css/ionicons.css",
+                      "~/Content/AdminLTE.css",
+                      "~/Content/skins/_all-skins.css",
                       "~/Content/sprites.css",
                       "~/Content/site.css"));
         }
